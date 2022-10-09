0b88c843658e41b2b90e4e46bbb23745eee1d81a
huubl <>
2022-10-09 23:02:18 +0200

    1.0.3

--- user-switching-fix.php
+++ user-switching-fix.php
@@ -1,26 +1,30 @@
 <?php
 /**
  * Plugin Name:  User Switching when Wordpress is in subdirectory
- * Plugin URI:   https://github.com/roots/bedrock-autoloader
+ * Plugin URI:   https://github.com/smeedijzer-online/smeedijzer-mu-user-switching-fix
  * Description:  Fix: Switch back fails when Wordpress is in subdirectory and using custom login page. See: https://github.com/johnbillion/user-switching/issues/14
- * Version:      1.0.0
+ * Version:      1.0.3
  * Author:       Smeedijzer Internet
  * Author URI:   https://www.smeedijzer.net
  * License:      MIT License
  */
 
 add_action('wp_die_handler', function($handler) {
-    parse_str($_SERVER['QUERY_STRING'], $query);
+	if( !isset($query['action'], $_SERVER['QUERY_STRING']) ) {
+		return $handler;
+	}
 
-    if ( isset($query['action']) && $query['action'] === 'switch_to_olduser' ) {
-        wp_redirect(get_bloginfo('wpurl') . $_SERVER['REQUEST_URI']);
-        exit;
-    }
+	parse_str($_SERVER['QUERY_STRING'], $query);
 
-    if ( isset($query['action']) && $query['action'] === 'switch_to_user' ) {
-        wp_redirect(get_bloginfo('wpurl') . $_SERVER['REQUEST_URI']);
-        exit;
-    }
+	if( $query['action'] === 'switch_to_olduser' ) {
+		wp_redirect(get_bloginfo('wpurl') . $_SERVER['REQUEST_URI']);
+		exit;
+	}
 
-    return $handler;
+	if( $query['action'] === 'switch_to_user' ) {
+		wp_redirect(get_bloginfo('wpurl') . $_SERVER['REQUEST_URI']);
+		exit;
+	}
+
+	return $handler;
 });
