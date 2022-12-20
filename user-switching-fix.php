04db12459ff610422cd835db85e655b534722d47
huubl <50170696+huubl@users.noreply.github.com>
2022-12-20 14:22:34 +0100

    1.0.4

--- user-switching-fix.php
+++ user-switching-fix.php
@@ -3,18 +3,22 @@
  * Plugin Name:  User Switching when Wordpress is in subdirectory
  * Plugin URI:   https://github.com/smeedijzer-online/smeedijzer-mu-user-switching-fix
  * Description:  Fix: Switch back fails when Wordpress is in subdirectory and using custom login page. See: https://github.com/johnbillion/user-switching/issues/14
- * Version:      1.0.3
+ * Version:      1.0.4
  * Author:       Smeedijzer Internet
  * Author URI:   https://www.smeedijzer.net
  * License:      MIT License
  */
 
 add_action('wp_die_handler', function($handler) {
-	if( !isset($query['action'], $_SERVER['QUERY_STRING']) ) {
+	if( !isset($_SERVER['QUERY_STRING']) ) {
 		return $handler;
 	}
 
 	parse_str($_SERVER['QUERY_STRING'], $query);
+
+	if( !isset($query['action']) ) {
+		return $handler;
+	}
 
 	if( $query['action'] === 'switch_to_olduser' ) {
 		wp_redirect(get_bloginfo('wpurl') . $_SERVER['REQUEST_URI']);
